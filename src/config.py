4c549ec3130fb972e9162485cbf5414ef8911c69
kolenkoal <kolenko.alexander0@gmail.com>
2024-01-11 18:13:50 +0300

    Added pre-commit

---
+++ src/config.py
@@ -0,0 +1,15 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    DB_HOST: str
+    DB_PORT: int
+    DB_USER: str
+    DB_PASSWORD: str
+    DB_NAME: str
+
+    TEST_DB_HOST: str
+    TEST_DB_PORT: int
+    TEST_DB_USER: str
+    TEST_DB_PASS: str
+    TEST_DB_NAME: str
--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
 
+
 app = FastAPI()
 
 
 @app.get("/")
 def hello():
-    return {"hello": "world"}
+    return {"hello": "world          "}
